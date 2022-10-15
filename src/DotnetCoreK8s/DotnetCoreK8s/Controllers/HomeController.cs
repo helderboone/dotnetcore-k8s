8236f3aa16f195d383fd1fc542c024c8c9824fec
Helder Boone Borges <Helder.Borges@ey.com>
2022-10-15 11:22:46 -0300

    Initial commit

---
+++ src/DotnetCoreK8s/DotnetCoreK8s/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreK8s.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(long id)
+        {
+            return Ok(new { id });
+        }
+    }
+}
